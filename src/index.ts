29991f98dde19bb527fc5843d30d3506687eef55
Tharindu Lakmal <lakmaltharindu768@gmail.com>
2024-02-28 15:39:33 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import notificationTemplate from "./adaptiveCards/notification-default.json";
 import { notificationApp } from "./internal/initialize";
 import { CardData } from "./cardModels";
 import { TeamsBot } from "./teamsBot";
+import { ActivityTypes, BotHandler, TeamsActivityHandler, TurnContext } from "botbuilder";
 
 // Create HTTP server.
+const teamsBot = new TeamsBot();
+
+
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
@@ -24,76 +28,52 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 // You can add authentication / authorization for this API. Refer to
 // https://aka.ms/teamsfx-notification for more details.
 
-function extractChannelIdFromTeamsUrl(url) {
-  const regex = /\/channel\/([^\/?]+)/;
-  const match = url.match(regex);
-
-  if (match && match[1]) {
-    // Decode the extracted channel ID
-
-    const channelId = decodeURIComponent(match[1]);
-    return channelId;
-  } else {
-    // Return null or handle the case when the channel ID is not found
-    return null;
-  }
-}
 server.post(
   "/api/notification",
   restify.plugins.queryParser(),
   restify.plugins.bodyParser(), // Add more parsers if needed
   async (req, res) => {
-    // By default this function will iterate all the installation points and send an Adaptive Card
-    // to every installation.
-    const pageSize = 100;
-    let continuationToken: string | undefined = undefined;
+   try {
+     //await notificationApp.requestHandler(req, res, async (context) => {
+        const channelId =
+          "19:m4dMACXOeQJChaNf8jCP0zkfQUKtvVMKaS5qL5mPllw1@thread.tacv2";
+    
+    
+       //  const adapter = notificationApp.adapter; // Assuming this is available
+       //  const context = new TurnContext(adapter, req);
 
-    const link =
-      "https://teams.microsoft.com/l/channel/19%3arThf8rXn9SuHW9HXa_7ubHpdlRtafFrcbn76rANtN681%40thread.tacv2/General?groupId=b21eba14-2733-4976-9e8e-9e046a9c340e&tenantId=58ccd57c-6495-4bc8-842a-c181885cea2b";
-
-    const channelId = extractChannelIdFromTeamsUrl(link);
-
-    // Have to add our own channel id here for testing
-    const mockChannelID =
-      "19%3arThf8rXn9SuHW9HXa_7ubHpdlRtafFrcbn76rANtN681%40thread.tacv2";
-
-    const channelData = await notificationApp.notification.findChannel(
-      async (channel) => channel.info.id === mockChannelID
-    );
-
-    channelData?.sendAdaptiveCard(req.body);
-
-    // do {
-    //   const pagedData =
-    //     await notificationApp.notification.getPagedInstallations(
-    //       pageSize,
-    //       continuationToken
-    //     );
-
-    //   const installations = pagedData.data;
-
-    //   continuationToken = pagedData.continuationToken;
-
-    //   for (const target of installations) {
-    //     await target.sendAdaptiveCard(
-    //       req.body?.formattedCardPayload?.attachments?.[0]?.content
-    //     );
-    //   }
-    // } while (continuationToken);
+       //console.log("im running");
+       //await teamsBot.sendMessageToChannel(channelId, "message", context);
+    // });
+   } catch (e) {
+     console.log("noti :", e);
+   }
 
     res.json({});
   }
 );
 
-// Register an API endpoint with `restify`. Teams sends messages to your application
-// through this endpoint.
-//
-// The Teams Toolkit bot registration configures the bot with `/api/messages` as the
-// Bot Framework endpoint. If you customize this route, update the Bot registration
-// in `/templates/provision/bot.bicep`.
-const teamsBot = new TeamsBot();
-server.post("/api/messages", async (req, res) => {
+server.post("/api/messages"  ,
+restify.plugins.queryParser(),
+restify.plugins.bodyParser(), // Add more parsers if needed, 
+  async (req, res) => {
+console.log('req :', req);
+try{
   await notificationApp.requestHandler(req, res, async (context) => {
+    console.log("context :", context);
     await teamsBot.run(context);
+    console.log("im running");
+    const channelId =
+      "19:m4dMACXOeQJChaNf8jCP0zkfQUKtvVMKaS5qL5mPllw1@thread.tacv2";
+    //  const adapter = notificationApp.adapter; // Assuming this is available
+    //  const context = new TurnContext(adapter, req);
+
+    //console.log("im running");
+    //await teamsBot.sendMessageToChannel(channelId, "message", context);
   });
+}
+catch(e){
+console.log('e :', e);
+
+}
 });
--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,32 +1,63 @@
-import { ActivityTypes, TeamsActivityHandler, TurnContext } from "botbuilder";
+import { TeamsUserCredential } from "@microsoft/teamsfx";
+import { ActivityEventNames, ActivityTypes, InstallationUpdateActionTypes, MessageFactory, TeamsActivityHandler, TeamsInfo, TurnContext, teamsGetChannelId } from "botbuilder";
+import { debug } from "console";
+import { debuglog } from "util";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
 export class TeamsBot extends TeamsActivityHandler {
+
   constructor() {
     super();
+     this.onMessage(async (context, next) => {
+       const teamsChannelId = teamsGetChannelId(context.activity);
+           const channelId =
+             "19:m4dMACXOeQJChaNf8jCP0zkfQUKtvVMKaS5qL5mPllw1@thread.tacv2";
+
+       const activity = {
+         type: ActivityTypes.Message,
+         teamsChannelId,
+         text: "hey i'm working",
+       };
+      await context.sendActivity(activity);
+       await next();
+     });
   }
-  protected onTurnActivity(context: TurnContext): Promise<void> {
-    // Add your custom logic here
-
-    if (context.activity.type === ActivityTypes.ConversationUpdate) {
-      if (
-        context.activity.membersAdded.some(
-          (member) => member.id === context.activity.recipient.id
-        )
-      ) {
-        const teamId = context.activity.conversation.id;
-        const channelData = context.activity.channelData;
-        const channelId = channelData.channel.id;
-        // Store or use the channelId as needed
-
-        console.log(
-          `Bot added to channel:
-${channelId}
-`
-        );
-      }
+  protected async onInstallationUpdateActivity(
+    context: TurnContext
+  ): Promise<void> {
+    console.log(context.activity.action);
+    if (
+      context.activity.type == ActivityTypes.InstallationUpdate &&
+      context.activity.action === InstallationUpdateActionTypes.Add
+    ) {
+      console.log("added :", context.activity.channelData);
+      console.log("added :", context.activity.channelId);
+      // this.sendMessageToChannel(context.activity.channelId,'bla bla blaa',context);
+    } else if (
+      context.activity.type == ActivityTypes.InstallationUpdate &&
+      context.activity.action === InstallationUpdateActionTypes.Remove
+    ) {
+      console.log("removed :", context.activity);
     }
-    return;
+
+    console.log("done");
+  }
+
+  async sendMessageToChannel(
+    channelId: string,
+    message: string,
+    context
+  ): Promise<void> {
+    try {
+      const activity = {
+        type: ActivityTypes.Message,
+        channelId,
+        text: message,
+      };
+      await context.sendActivity(activity);
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   }
 }
